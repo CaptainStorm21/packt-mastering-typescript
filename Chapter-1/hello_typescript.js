726cf19a846f2053b80112d768cd5a5e55af6e5f
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-04-23 03:46:48 -0700

    chapter 1 - basics

---
+++ Chapter-1/hello_typescript.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log(`hello from typescript`);
+var version = `es6`;
+console.log(`hello ${version} Typescript`);
---
+++ Chapter-1/hello_typescript.ts
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log(`hello from typescript`)
+
+var version = `es6`
+console.log(`hello ${version} Typescript`)
+
